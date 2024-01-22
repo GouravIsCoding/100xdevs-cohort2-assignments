e4e5193ccb04901f5d9a06eb968e86fc029b4dc6
GouravisCoding <pythonicgourav@gmail.com>
2024-01-23 00:30:44 +0530

    minor changes in  week 7

--- week-7/week7Assignment/src/App.jsx
+++ week-7/week7Assignment/src/App.jsx
@@ -10,18 +10,18 @@ import {
 function App() {
   return (
     <>
-      {/* <Question1
+      <Question1
         followers={100}
         likes={1400}
         photos={8000}
         name={"Gourav"}
         id={"GouravIsCoding"}
-      /> */}
+      />
 
       {/* <Question2 /> */}
       {/* <Question4 /> */}
       {/* <Question5 /> */}
-      <Question6 />
+
       {/* <Qustions7 /> */}
     </>
   );
--- week-7/week7Assignment/src/questions/index.js
+++ week-7/week7Assignment/src/questions/index.js
@@ -2,6 +2,6 @@ import Question1 from "./Question1";
 import Question2 from "./Question2";
 import Question4 from "./Question4";
 import Question5 from "./Question5";
-import Question6 from "./Question6";
+
 import Qustions7 from "./Qustions7";
-export { Question1, Question2, Question4, Question5, Question6, Qustions7 };
+export { Question1, Question2, Question4, Question5, Qustions7 };
